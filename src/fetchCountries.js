dd9ea556b5cb4b5b54ea80a3c1c331b24541de37
Mykkolla <tarabukin.n.10@gmail.com>
2023-04-08 11:28:10 +0300

    add

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,54 @@
-// export const fetchCountries = name => {
+import axios from 'axios';
 
-//     const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
-//       return fetch(url).then(res => {
-//             if (!res.ok) {
-//                 throw new Error(response.status);
-//             }
-//             return res.json()
-//         })
-//     }
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.PER_PAGE = 40;
+  }
+  async fetchGallery() {
+    const axiosOptions = {
+      method: 'get',
+      url: 'https://pixabay.com/api/',
+      params: {
+        key: '34523545-f21683fd59bfc3e4e2549fe07',
+        q: `${this.searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: `${this.page}`,
+        per_page: `${this.PER_PAGE}`,
+      },
+    };
+    try {
+      const response = await axios(axiosOptions);
+
+      const data = response.data;
+
+      this.incrementPage();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  resetEndOfHits() {
+    this.endOfHits = false;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,153 +1,109 @@
-import Notiflix from 'notiflix';
-const axios = require('axios');
-const debounce = require('lodash.debounce');
-import { fetchCountries } from './fetchCountries.js';
+import './sass/index.scss';
+import NewsApiService from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import './css/styles.css';
-
-
-document.getElementById('search-form').addEventListener('submit', handleLoadMoreButtonClick)
-const gallery = document.querySelector('.gallery');
-// const loadMoreBtn = document.querySelector('.load-more')
-
-const API_KEY = '35143561-a246dd3ff16ac48132d2e40aa'; // Замініть на свій API-ключ Pixabay
-const MAX_PER_PAGE = 40; // Максимальна кількість зображень на сторінці
-let currentPage = 1; // Поточна сторінка
-let currentKeyword = ''; // Поточне ключове слово
-
-async function handleLoadMoreButtonClick(event) {
-  event.preventDefault();
-  const searchQuery = event.target.elements.searchQuery.value;
-  if (searchQuery === '') {
-    alert('Please enter a search query');
-    return;
-}
-  try {
-   const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&page=${currentPage}&per_page=${MAX_PER_PAGE}`
-  );
-  const data = await response.json();
-  if (data.hits.length === 0) {
-  gallery.innerHTML = '<p>Sorry, there are no images matching your search query. Please try again.</p>';
-}
-else {renderImageCards(data.hits);}
-} 
-catch (error) {
-      console.error('Error fetching images:', error);
-      return null;
-}
-};
-
-function renderImageCards(images) {
-
-  gallery.innerHTML = '';
-  images.forEach(image => {
-    const imageCard = `
-      <div class="photo-card">
-        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-        <div class="info">
-          <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-          <p class="info-item"><b>Views:</b> ${image.views}</p>
-          <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-          <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
-        </div>
-      </div>
-    `;
-    
-    gallery.insertAdjacentHTML('beforeend', imageCard);
+let lightbox = new SimpleLightbox('.photo-card a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
   });
+ 
+  const searchForm = document.querySelector('.search-form');
+  const galleryContainer = document.querySelector('.gallery');
+  const loadMoreBtn = document.querySelector('.load-more');
+
+let isShown = 0;
+const newsApiService = new NewsApiService();
+
+searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.style.display = 'none';
+const options = {
+  rootMargin: '50px',
+  root: null,
+  threshold: 0.3,
+};
+new IntersectionObserver(onLoadMore, options);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  galleryContainer.innerHTML = '';
+  newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  newsApiService.resetPage();
+
+  if (newsApiService.query === '') {
+    Notify.warning('Please, fill the main field');
+    return;
+  }
+
+  isShown = 0;
+  fetchGallery();
+  onRenderGallery(hits);
 }
 
-const button = document.querySelector('.load-more');
-    button.addEventListener('click', loadMore);
-
-
-
-function createImageCard(image) {
-  const card = document.createElement('div');
-  card.className = 'photo-card';
-
-  const img = document.createElement('img');
-  img.src = image.webformatURL;
-  img.alt = image.tags;
-  img.loading = 'lazy';
-
-  const info = document.createElement('div');
-  info.className = 'info';
-
-  const likes = document.createElement('p');
-  likes.className = 'info-item';
-  likes.innerHTML = `<b>Likes:</b> ${image.likes}`;
-
-  const views = document.createElement('p');
-  views.className = 'info-item';
-  views.innerHTML = `<b>Views:</b> ${image.views}`;
-
-  const comments = document.createElement('p');
-  comments.className = 'info-item';
-  comments.innerHTML = `<b>Comments:</b> ${image.comments}`;
-
-  const downloads = document.createElement('p');
-  downloads.className = 'info-item';
-  downloads.innerHTML = `<b>Downloads:</b> ${image.downloads}`;
-
-  info.appendChild(likes);
-  info.appendChild(views);
-  info.appendChild(comments);
-  info.appendChild(downloads);
-
-  card.appendChild(img);
-  card.appendChild(info);
-
-  return card;
+function onLoadMore() {
+  newsApiService.incrementPage();
+  fetchGallery();
 }
 
-// axios.get('https://pixabay.com/api/', {
-//   params: {
-//     key: apiKey,
-//     q: searchQuery,
-//     image_type: imageType,
-//     orientation: orientation,
-//     safesearch: safeSearch,
-//     per_page: perPage,
-//     page: page
-//   }
-// })
-// .then(response => {
-//   // Обробка відповіді
-//   const images = response.data.hits; // Масив зображень
-//   if (images.length === 0) {
-//     // Показ повідомлення про відсутність зображень
-//     console.log("Sorry, there are no images matching your search query. Please try again.");
-//   } else {
-//     // Відображення зображень на сторінці
-//     images.forEach(image => {
-//       // Отримання необхідних властивостей зображення
-//       const webformatURL = image.webformatURL;
-//       const largeImageURL = image.largeImageURL;
-//       const tags = image.tags;
-//       const likes = image.likes;
-//       const views = image.views;
-//       const comments = image.comments;
-//       const downloads = image.downloads;
+async function fetchGallery() {
+  loadMoreBtn.style.display = 'none';
+  const gar = await newsApiService.fetchGallery();
+  const { hits, total } = gar;
+  isShown += hits.length;
+  if (!hits.length) {
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+    loadMoreBtn.style.display = 'none';
+    return;
+  }
+  onRenderGallery(hits);
+  isShown += hits.length;
+  if (isShown < total) {
+    Notify.success(`Hooray! We found ${total} images !!!`);
+    loadMoreBtn.style.display = 'block';
+  }
+  if (isShown >= total) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
 
-//       // Додавання розмітки картки зображення до галереї
-//       const gallery = document.querySelector('.gallery');
-//       const photoCard = document.createElement('div');
-//       photoCard.classList.add('photo-card');
-//       photoCard.innerHTML = `
-//         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//         <div class="info">
-//           <p class="info-item"><b>Likes:</b> ${likes}</p>
-//           <p class="info-item"><b>Views:</b> ${views}</p>
-//           <p class="info-item"><b>Comments:</b> ${comments}</p>
-//           <p class="info-item"><b>Downloads:</b> ${downloads}</p>
-//         </div>
-//       `;
-//       gallery.appendChild(photoCard);
-//     });
-//   }
-// })
-// .catch(error => {
-//   // Обробка помилки
-//   console.error('Error fetching images:', error);
-// });
+function onRenderGallery(elements) {
+  const markup = elements
+    .map(
+      ({webformatURL,largeImageURL,tags,
+      likes,views,comments,downloads,
+      }) => {
+        return `<div class="photo-card">
+    <a href="${largeImageURL}">
+      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${downloads}
+      </p>
+    </div>
+    </div>`;
+      }
+    )
+    .join('');
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
